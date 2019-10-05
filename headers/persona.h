5393f7dab7526a15c4e6c14c14ffdcc8a6cbb3eb
Renzo <renzojimenezalfarotrabajos@gmail.com>
2019-10-05 12:13:57 -0500

    "Agregadno Persona.cpp y persona.h"

--- headers/persona.h
+++ headers/persona.h
@@ -16,10 +16,5 @@ Persona::Persona(char*Nombres_persona,int Edad_persona){
 	Nombres=Nombres_persona;
 	Edad=Edad_persona;
 }
-char * Persona::getNombres(){return Nombres;}
-int Persona::getEdad(){return Edad;}
-void Personas::Persona::setNombres(char* Nombre_persona){Nombres=Nombre_persona;}
-void Persona::setEdad(int Edad){Edad=Edad;}
-void Persona::MostrarInfo(){}
 }
 #endif
--- src/Persona.cpp
+++ src/Persona.cpp
@@ -4,5 +4,13 @@
 using namespace std;
 using namespace Personas;
 
-
+char * Persona::getNombres(){return Nombres;}
+int Persona::getEdad(){return Edad;}
+void Personas::Persona::setNombres(char* Nombre_persona){Nombres=Nombre_persona;}
+void Persona::setEdad(int Edad){Edad=Edad;}
+void Persona::MostrarInfo(){
+	cout<<"Persona "<<endl;
+	cout<<"Nombre: "<<Persona::getNombres();
+	cout<<"Edad: "<<Persona::getEdad();
+}
 
