5775a324e3eea6e830ec1341892915611416b679
Renzo <renzojimenezalfarotrabajos@gmail.com>
2019-10-04 20:51:10 -0500

    Agregando Structura del Proyecto

---
+++ Refactoring_Sample_Cpp/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include<C:\Users\hijo7\Desktop\Villos\Src\Lista.cpp>
+
+using namespace std;
+
+int main(){
+	Mostrar();
+}
---
+++ Refactoring_Sample_Cpp/Src/Lista.cpp
@@ -0,0 +1,6 @@
+#include <C:\Users\hijo7\Desktop\Villos\Headers\Lista.h>
+#include <iostream>
+using namespace std;
+void Mostrar(){
+	ShowMessage();
+}
